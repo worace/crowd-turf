1dae2cd87b0fa11dfd3ae7aff0b67bc6624e6cde
Horace Williams <horace.d.williams@gmail.com>
2017-04-13 22:01:42 -0700

    GeoJSON rendering proof of concept

--- front/src/state.js
+++ front/src/state.js
@@ -1,7 +1,43 @@
 import Imm from 'immutable';
 
 const initialState = Imm.Map({
-  center: Imm.Map({latitude: 34.0522, longitude: -118.2437})
+  center: Imm.Map({latitude: 34.0522, longitude: -118.2437}),
+  currentFeature: {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [
+            [
+              [
+                -118.27983856201172,
+                34.04569008803844
+              ],
+              [
+                -118.26009750366211,
+                34.04569008803844
+              ],
+              [
+                -118.26009750366211,
+                34.060197034932095
+              ],
+              [
+                -118.27983856201172,
+                34.060197034932095
+              ],
+              [
+                -118.27983856201172,
+                34.04569008803844
+              ]
+            ]
+          ]
+        }
+      }
+    ]
+  }
 });
 
 export default initialState;
