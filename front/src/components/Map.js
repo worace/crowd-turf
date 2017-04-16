75019409792b3b589594d10f7324250db3eb5c0d
Horace Williams <horace.d.williams@gmail.com>
2017-04-16 16:12:06 -0700

    Clean up Map code and extract into its own component namespace

---
+++ front/src/components/Map.js
@@ -0,0 +1,57 @@
+import mapboxgl from 'mapbox-gl';
+import MapboxDraw from '@mapbox/mapbox-gl-draw';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoid29yYWNlIiwiYSI6ImNqMHEzcmpqNzAxbGwzM281bHQ3dDBsOXIifQ.75hrCmvGH7KVs2Hyl86pzw';
+
+function updateMap(map, state) {
+  map.getSource('turfSet').setData(state.get('turfSet').toJS());
+}
+
+const turfLayer = {
+  'id': 'turfSet',
+  'type': 'fill',
+  'source': 'turfSet',
+  'layout': {},
+  'paint': {
+    'fill-color': '#33FFFC',
+    'fill-opacity': 0.8
+  }
+};
+
+function initSources(map, state) {
+  map.addSource('turfSet',
+                {type: 'geojson',
+                 data: state.get('turfSet').toJS()});
+  map.addLayer(turfLayer);
+}
+
+function initDispatch(map, store) {
+  map.on('draw.create', ({features}) => store.dispatch({type: 'FEATURES_ADDED', payload: features}));
+}
+
+function initSubscriptions(map, store) {
+  store.subscribe(() => updateMap(map, store.getState()));
+}
+
+function init(store) {
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v8',
+    center: [-118.5884298, 34.3921461],
+    zoom: 17
+  });
+
+  const draw = new MapboxDraw({displayControlsDefault: false});
+  map.addControl(draw);
+
+  map.on('load', () => {
+    initSources(map, store.getState());
+    initDispatch(map, store);
+    initSubscriptions(map, store);
+    draw.changeMode(draw.modes.DRAW_POLYGON);
+    store.dispatch({type: 'MAP_LOADED',
+                    payload: {map, drawControl: draw}});
+  });
+}
+
+export default {init};
